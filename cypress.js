e4671f7c098ad45c54387202e4df1c25e51b7cdc
Richard Forjoe <richard.forjoe@sky.uk>
2021-12-27 07:20:05 +0000

    chore(automation): pipeline setup

--- cypress.js
+++ cypress.js
@@ -1,15 +1,13 @@
 const cypress = require('cypress');
 const yargs = require('yargs');
 
-
 // https://filiphric.com/test-grepping-in-cypress
 
-const { grep } = yargs
-  .option('grep', {
-    type: 'array',
-    default: ['*']
-  }).argv;
+const { grep } = yargs.option('grep', {
+  type: 'array',
+  default: ['*'],
+}).argv;
 
-  cypress.run({
+cypress.run({
   spec: grep.map(folder => `./cypress/integration/${folder}/*.*`),
 });
